1d364f21d29c879150b0de1c864aee2b0f51ecc3
maayan <mshnaider@campus.technion.ac.il>
2024-08-13 14:55:20 +0300

    runtime exception

--- Park.java
+++ Park.java
@@ -1,26 +1,28 @@
+import java.util.ArrayList;
+
 public class Park {
     private String name;
-    private AmusementRide[] rides;
+    private ArrayList<AmusementRide> rides;
     private int rideCount;
     private static final int MAX_RIDES = 5;
 
     public Park(String name) {
         this.name = name;
-        this.rides = new AmusementRide[MAX_RIDES];
+        this.rides = new ArrayList<AmusementRide>();
         this.rideCount = 0;
     }
 
     public void add(AmusementRide ride) {
         if (rideCount < MAX_RIDES) {
-            rides[rideCount++] = ride;
+            rides.set(rideCount++, ride);
         }
     }
 
     public void remove(AmusementRide ride) {
         for (int i = 0; i < rideCount; i++) {
-            if (rides[i] == ride) {
-                System.arraycopy(rides, i + 1, rides, i, rideCount - i - 1);
-                rides[--rideCount] = null;
+            if (rides.get(i) == ride) {
+                rides.remove(i);
+                --rideCount;
                 return;
             }
         }
@@ -28,14 +30,14 @@ public class Park {
 
     public void startRides() throws EmptyQueueException {
         for (int i = 0; i < rideCount; i++) {
-            rides[i].startRide();
+            rides.get(i).startRide();
         }
     }
 
     public void addPerson(AmusementRide ride, Person person) throws InvalidInputException {
         for (int i = 0; i < rideCount; i++) {
-            if (rides[i] == ride) {
-                rides[i].getQueue().add(person);
+            if (rides.get(i) == ride) {
+                rides.get(i).getQueue().add(person);
                 return;
             }
         }
