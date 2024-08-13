147eddddfebcbbc1471a97b00658a92404ae1a86
samis <samuelas@campus.technion.ac.il>
2024-08-13 12:01:21 +0300

    handle exceptions 3

--- InvalidInputException.java
+++ InvalidInputException.java
@@ -1,5 +1,10 @@
 public class InvalidInputException extends IsraeliQueueException {
+    public InvalidInputException(){}
     public InvalidInputException(String message) {
         super(message);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
