72ff1dfe2cfbaeadf66decef88d72290707ede3a
samis <samuelas@campus.technion.ac.il>
2024-08-18 17:59:12 +0300

    fix park to linked list 2

--- Park.java
+++ Park.java
@@ -1,21 +1,11 @@
 public class Park {
     private final String name;
-    private Node head;
-    private Node tail;
+    private Node<AmusementRide> head;
+    private Node<AmusementRide> tail;
     private int rideCount;
     private static final int MAX_RIDES = 5;
 
-    // Private inner Node class
-    private static class Node {
-        AmusementRide ride;
-        Node next;
-
-        Node(AmusementRide ride) {
-            this.ride = ride;
-            this.next = null;
-        }
-    }
-
+    // Constructor
     public Park(String name) {
         this.name = name;
         this.head = null;
@@ -23,32 +13,34 @@ public class Park {
         this.rideCount = 0;
     }
 
+    // Add a ride to the park
     public void add(AmusementRide ride) {
         if (rideCount < MAX_RIDES) {
-            Node newNode = new Node(ride);
+            Node<AmusementRide> newNode = new Node<>(ride);
             if (tail == null) {
                 head = tail = newNode;
             } else {
-                tail.next = newNode;
+                tail.setNext(newNode);
                 tail = newNode;
             }
             rideCount++;
         }
     }
 
+    // Remove a ride from the park
     public void remove(AmusementRide ride) {
-        Node current = head;
-        Node previous = null;
+        Node<AmusementRide> current = head;
+        Node<AmusementRide> previous = null;
 
         while (current != null) {
-            if (current.ride == ride) {
+            if (current.getValue() == ride) {
                 if (previous == null) {
-                    head = current.next;
+                    head = current.getNext();
                     if (head == null) {
                         tail = null;
                     }
                 } else {
-                    previous.next = current.next;
+                    previous.setNext(current.getNext());
                     if (current == tail) {
                         tail = previous;
                     }
@@ -57,26 +49,28 @@ public class Park {
                 return;
             }
             previous = current;
-            current = current.next;
+            current = current.getNext();
         }
     }
 
+    // Start all rides in the park
     public void startRides() throws EmptyQueueException {
-        Node current = head;
+        Node<AmusementRide> current = head;
         while (current != null) {
-            current.ride.startRide();
-            current = current.next;
+            current.getValue().startRide();
+            current = current.getNext();
         }
     }
 
+    // Add a person to a specific ride's queue
     public void addPerson(AmusementRide ride, Person person) throws InvalidInputException {
-        Node current = head;
+        Node<AmusementRide> current = head;
         while (current != null) {
-            if (current.ride == ride) {
-                current.ride.getQueue().add(person);
+            if (current.getValue() == ride) {
+                current.getValue().getQueue().add(person);
                 return;
             }
-            current = current.next;
+            current = current.getNext();
         }
     }
 }
